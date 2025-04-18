cc64caed268c562cba250e14669d9f23f39b28a4
isa-leroux448 <isa.leroux448@gmail.com>
2025-04-18 15:42:12 -0700

    reorganize pages

--- src/app/create-account/player/page.tsx
+++ src/app/create-account/player/page.tsx
@@ -1,8 +1,8 @@
-export default function RequestAccount() {
+export default function CreateAccount() {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-2 sm:p-10 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-start sm:items-start">
-          <p>Coach Request Account Page</p>
+          <p>Create account form</p>
         </main>
       </div>
     );
--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -1,8 +1,8 @@
-export default function CreateAccount() {
+export default function Player() {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-2 sm:p-10 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-start sm:items-start">
-          <p>Select player or coach</p>
+          <p>Redirect to player or coach page</p>
         </main>
       </div>
     );

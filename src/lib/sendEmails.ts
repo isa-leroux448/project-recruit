2ed72baa5fa8a2041344fe8453b36189ddf34881
isa-leroux448 <isa.leroux448@gmail.com>
2025-04-18 11:36:41 -0700

    add log statements to email

--- src/lib/sendEmails.ts
+++ src/lib/sendEmails.ts
@@ -10,7 +10,7 @@ export async function sendAccountRequest(coachInfo: CoachType) {
     key: process.env.MAILGUN_API_KEY as string
   });
   try {
-    await mg.messages.create(DOMAIN as string, {
+    const data = await mg.messages.create(DOMAIN as string, {
       from: `Project Recruit <noreply@${DOMAIN}>`,
       to: (process.env.ADMIN_EMAILS?.split(',').map(email => email.trim()) || ["Isabella Leroux <isabella.leroux.dev@gmail.com>"]),
       subject: `New account request - ${coachInfo.firstName} ${coachInfo.lastName}`,
@@ -23,6 +23,7 @@ export async function sendAccountRequest(coachInfo: CoachType) {
         position: coachInfo.position
       }),
     });
+    console.log(data)
   } catch (error) {
     console.log(error);
   }
@@ -37,7 +38,7 @@ export async function sendDecisionEmail(coachInfo: CoachType, status: "approved"
   try {
     const subject = status === "approved" ? "Your account has been approved!" : "Your account request has been denied";
     const template = status === "approved" ? "account approved" : "account denied";
-    await mg.messages.create(DOMAIN as string, {
+    const data = await mg.messages.create(DOMAIN as string, {
       from: `Project Recruit <noreply@${DOMAIN}>`,
       to: [`${coachInfo.firstName} ${coachInfo.lastName} <${coachInfo.email}>`],
       subject,
@@ -50,6 +51,7 @@ export async function sendDecisionEmail(coachInfo: CoachType, status: "approved"
         position: coachInfo.position
       }),
     });
+    console.log(data)
   } catch (error) {
     console.log(error);
   }

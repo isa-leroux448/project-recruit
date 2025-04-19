6136122067037df2cc49a8ef8cafab2d50705981
isa-leroux448 <isa.leroux448@gmail.com>
2025-04-19 11:44:59 -0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,39 @@
-import { NextRequest, NextResponse } from "next/server"
-import { auth0 } from "./lib/auth0";
+import { NextRequest, NextResponse } from "next/server";
+import { auth0 } from "./lib/auth0"
 
 export async function middleware(request: NextRequest) {
     const authRes = await auth0.middleware(request);
 
-    const protectedRoutes = ["/player", "/coach", "/redirect"];
-    const isProtectedRoute = protectedRoutes.some(route => request.nextUrl.pathname.startsWith(route));
+    // authentication routes — let the middleware handle it
+    if (request.nextUrl.pathname.startsWith("/auth")) {
+        return authRes;
+    }
+    
+    // public routes — no need to check for session
+    if (request.nextUrl.pathname === ("/") || request.nextUrl.pathname === ("/create-account")) {
+        return authRes;
+    }
 
-    const session = await auth0.getSession(request)
+    const { origin } = new URL(request.url)
+    const session = await auth0.getSession()
 
-    if (!session && isProtectedRoute) {
-        return NextResponse.redirect(new URL("/auth/login", request.nextUrl.origin))
+    // user does not have a session — redirect to login
+    if (!session) {
+        return NextResponse.redirect(`${origin}/auth/login`)
     }
 
     return authRes
-  }
+}
 
-  
-//   export const config = {
-//     matcher: [
-//       "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"
-//     ],
-//   };
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+         * - api (API routes)
+         */
+        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api).*)",
+    ],
+}

47cba75dfd875d1accd9c0577b07b0b6380beada
isa-leroux448 <isa.leroux448@gmail.com>
2025-04-19 10:31:53 -0700

    remove config matcher in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,8 +17,8 @@ export async function middleware(request: NextRequest) {
   }
 
   
-  export const config = {
-    matcher: [
-      "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"
-    ],
-  };
+//   export const config = {
+//     matcher: [
+//       "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"
+//     ],
+//   };
